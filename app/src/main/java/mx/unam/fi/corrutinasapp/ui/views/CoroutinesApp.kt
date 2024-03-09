f3d8875830aeaaeb252220a716e12b5f99d445f7
DanielAguirre19 <danielaguirrebravo@gmail.com>
2024-03-08 22:11:21 -0600

    1

---
+++ app/src/main/java/mx/unam/fi/corrutinasapp/ui/views/CoroutinesApp.kt
@@ -0,0 +1,95 @@
+package mx.unam.fi.corrutinasapp.ui.views
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import mx.unam.fi.corrutinasapp.R
+import mx.unam.fi.corrutinasapp.ui.theme.CorrutinasAppTheme
+import mx.unam.fi.corrutinasapp.viewmodel.MainViewModel
+
+@Composable
+fun CoroutinesApp(
+        viewModel: MainViewModel = MainViewModel(),
+        modifier: Modifier = Modifier
+    ){
+    var changeColor by remember{
+        mutableStateOf(false)
+    }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+        Button(
+            onClick = {changeColor = !changeColor},
+            colors = ButtonDefaults.buttonColors(
+                if (changeColor) Color.Blue else Color.Red
+            )
+        ) {
+            Text(text = stringResource(R.string.cambio_de_color))
+        }
+        Spacer(modifier = modifier.height(30.dp))
+
+        //Text(text = viewModel.resultState)
+        Spacer(modifier = modifier.height(30.dp))
+        Column(horizontalAlignment = Alignment.CenterHorizontally){
+            Text(text = "Contador 1", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+            Spacer(modifier = modifier.height(10.dp))
+            if (viewModel.resultState){
+                Text(text = "${viewModel.countTime} [s]", fontSize = 22.sp, color = Color.Red)
+            }else{
+                Text(text = "${viewModel.countTime} [s]", fontSize = 22.sp)
+            }
+
+
+            Spacer(modifier = modifier.height(30.dp))
+
+            Text(text = "Contador 2", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+            Spacer(modifier = modifier.height(10.dp))
+            if (viewModel.resultState2){
+                Text(text = "${viewModel.countTime2} [s]", fontSize = 22.sp, color = Color.Red)
+            }else{
+                Text(text = "${viewModel.countTime2} [s]", fontSize = 22.sp)
+            }
+        }
+
+        Spacer(modifier = modifier.height(30.dp))
+        Button(onClick = {viewModel.fetchData()}) {
+            Text(text = stringResource(R.string.realizar_consulta))
+        }
+        Spacer(modifier = modifier.height(10.dp))
+        Button(onClick = {viewModel.reset()}) {
+            Text(text = stringResource(R.string.detener_contadores))
+        }
+    }
+}
+
+@Preview()
+@Composable
+fun CoroutinesAppPreview(){
+    CorrutinasAppTheme {
+        CoroutinesApp()
+    }
+}
---
+++ app/src/main/java/mx/unam/fi/corrutinasapp/viewmodel/MainViewModel.kt
@@ -0,0 +1,64 @@
+package mx.unam.fi.corrutinasapp.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class MainViewModel: ViewModel(){
+    var resultState by mutableStateOf(false)
+        private set
+
+    var resultState2 by mutableStateOf(false)
+        private set
+
+    var countTime by mutableStateOf(0)
+        private set
+
+    var countTime2 by mutableStateOf(0)
+        private set
+
+
+    private var oneCount by mutableStateOf(false)
+
+    private var job: Job? = null
+
+
+    fun fetchData(){
+        job = viewModelScope.launch {
+            for (i in 1..5){
+                delay(1000)
+                countTime = i
+            }
+            if(countTime == 5){
+                resultState = true
+                for (i in 1..5){
+                    delay(1000)
+                    countTime2 = i
+                }
+            }
+            if(countTime2 == 5){
+                resultState2 = true
+            }
+            oneCount = true
+        }
+
+        if(oneCount){
+            job?.cancel()
+        }
+    }
+
+    fun reset() {
+        countTime = 0
+        countTime2 = 0
+        oneCount = false
+        resultState = false
+        resultState2 = false
+        job?.cancel()
+    }
+}
